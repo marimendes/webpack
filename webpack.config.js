15959d1184fd3f958c2599ec92abdf42661219f1
Mariana Mendes <mare.s.mendes@gmail.com>
2018-07-10 10:22:27 -0300

    Corrigindo configuração

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
-const webpack = require('estudo_webpack')
-
+const webpack = require('webpack')
 module.exports = {
-	entry: './ex/index.js',
-	output: { 
-		path: __dirname + '/public',
-		filename: '.bundle.js'
-	}
-	devServer: {
-		port: 8080,
-		contentBase: './public'
-	}
+  entry: './ex/index.js',
+  output: {
+  path: __dirname + '/public',
+  filename: './bundle.js'
+  },
+  devServer: {
+  port: 8080,
+  contentBase: './public'
+  }
 }
