ff421371581641fe90b0207af6a447f86bd03c37
Mariana Mendes <mare.s.mendes@gmail.com>
2018-07-10 10:59:58 -0300

    Exercicio 2

--- ex/index.js
+++ ex/index.js
@@ -1,2 +1,10 @@
-const logger = require('./logger')
-logger.info('Usando o padrão CommonJS!')
+class Pessoa {
+  constructor(nome) {
+  this.nome = nome
+  }
+  toString() {
+  return `Pessoa: ${this.nome}`
+  }
+}
+const pessoa = new Pessoa('Mariana')
+console.log(pessoa.toString())
