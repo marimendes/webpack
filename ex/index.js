22de9b41290647638312b195b8fb98a174e44569
Mariana Mendes <mare.s.mendes@gmail.com>
2018-07-12 09:56:41 -0300

    Adiciona o interpretador do Babel

--- ex/index.js
+++ ex/index.js
@@ -1,4 +1,13 @@
-import Pessoa from './pessoa'
+const produto = {
+	nome: 'Caneta Bic Preta',
+	preco: 1.90,
+	desconto: 0.05
+}
 
-const pessoa = new Pessoa('Mariana!!')
-console.log(pessoa.toString())
+function clone(objeto) {
+	return { ...objeto }
+}
+
+const novoProduto = clone(produto)
+novoProduto.nome = 'Caneta Bic Azul'
+console.log(produto, novoProduto)
