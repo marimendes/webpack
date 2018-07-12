be6e75847978b26e7ba4220f05fb5cc3380a11cc
Mariana Mendes <mare.s.mendes@gmail.com>
2018-07-12 09:12:21 -0300

    Configuração do Babel

--- ex/index.js
+++ ex/index.js
@@ -1,10 +1,4 @@
-class Pessoa {
-  constructor(nome) {
-  this.nome = nome
-  }
-  toString() {
-  return `Pessoa: ${this.nome}`
-  }
-}
-const pessoa = new Pessoa('Mariana')
+import Pessoa from './pessoa'
+
+const pessoa = new Pessoa('Mariana!!')
 console.log(pessoa.toString())
